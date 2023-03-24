f9db06d9ecff0ce444d0c1ab06b6a418fa58053b
hermeswing <hermeswing@naver.com>
2023-03-25 00:34:20 +0900

    Swagger Upgrade 3.0.0

--- src/main/java/octopus/config/SwaggerConfiguration.java
+++ src/main/java/octopus/config/SwaggerConfiguration.java
@@ -1,14 +1,7 @@
 package octopus.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.hateoas.client.LinkDiscoverer;
-import org.springframework.hateoas.client.LinkDiscoverers;
-import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
-import org.springframework.plugin.core.SimplePluginRegistry;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -21,23 +14,23 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
-	@Bean
-	public Docket swaggerApi() {
-		return new Docket(DocumentationType.SWAGGER_2).apiInfo(swaggerInfo()).select()
-				.apis(RequestHandlerSelectors.basePackage("octopus.backend")).paths(PathSelectors.any()).build()
-				.useDefaultResponseMessages(false); // 기본으로 세팅되는 200,401,403,404 메시지를 표시 하지 않음
-	}
-
-	private ApiInfo swaggerInfo() {
-		return new ApiInfoBuilder().title("Spring API Documentation").description("앱 개발시 사용되는 서버 API에 대한 연동 문서입니다")
-				.license("MIT License").licenseUrl("https://hermeslog.tistory.com/").version("1").build();
-	}
-
-	@Bean
-	public LinkDiscoverers discoverers() {
-		List<LinkDiscoverer> plugins = new ArrayList<>();
-		plugins.add(new CollectionJsonLinkDiscoverer());
-		
-		return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
-	}
+    @Bean
+    public Docket swaggerApi() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(swaggerInfo()).select()
+                .apis(RequestHandlerSelectors.basePackage("octopus.backend")).paths(PathSelectors.any()).build()
+                .useDefaultResponseMessages(false); // 기본으로 세팅되는 200,401,403,404 메시지를 표시 하지 않음
+    }
+    
+    private ApiInfo swaggerInfo() {
+        return new ApiInfoBuilder().title("Octopus API Documentation").description("앱 개발시 사용되는 서버 API에 대한 연동 문서입니다")
+                .license("MIT License").licenseUrl("https://hermeslog.tistory.com/").version("1.0").build();
+    }
+    
+    // @Bean
+    // public LinkDiscoverers discoverers() {
+    // List<LinkDiscoverer> plugins = new ArrayList<>();
+    // plugins.add(new CollectionJsonLinkDiscoverer());
+    //
+    // return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+    // }
 }
