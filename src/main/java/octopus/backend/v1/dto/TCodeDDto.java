5656e764bae6486eae03bb8efe7a2e9a3c779746
hermeswing <hermeswing@naver.com>
2023-04-01 01:17:25 +0900

    JsonProperty 추가

--- src/main/java/octopus/backend/v1/dto/TCodeDDto.java
+++ src/main/java/octopus/backend/v1/dto/TCodeDDto.java
@@ -4,6 +4,10 @@ import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -27,48 +31,58 @@ import octopus.entity.TCodeD;
 @NoArgsConstructor // 인자 없이 객체 생성
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true) // true의 경우 부모 클래스 필드 값들도 동일한지 체크하며, false(기본값)일 경우 자신 클래스의 필드 값만 고려한다.
+// @JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class) // Deprecated
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class TCodeDDto extends BaseDto {
-	private static final long serialVersionUID = 1L;
-
-	@NotNull
-	private String pCd;
-
-	@NotNull
-	private String cd;
-
-	private String cdNm;
-
-	@Size(min = 1, max = 1)
-	private String useYn;
-
-	@Digits(integer = 4, fraction = 0) // 정수 4자리, 소수점 0자리
-	private Integer sortSeq;
-
-	private String wdOpt1;
-	private String wdOpt2;
-	private String wdOpt3;
-	private String wdOpt4;
-	private String wdOpt5;
-
-	private Integer numOpt1;
-	private Integer numOpt2;
-	private Integer numOpt3;
-	private Integer numOpt4;
-	private Integer numOpt5;
-
-	private String rmk;
-
-	public static TCodeDDto makeDto(TCodeD code) {
-		return TCodeDDto.builder().pCd(code.getPCd()).cd(code.getCd()).cdNm(code.getCdNm()).sortSeq(code.getSortSeq())
-				.useYn(code.getUseYn()).wdOpt1(code.getWdOpt1()).wdOpt2(code.getWdOpt2()).wdOpt3(code.getWdOpt3())
-				.wdOpt4(code.getWdOpt4()).wdOpt5(code.getWdOpt5()).numOpt1(code.getNumOpt1()).numOpt2(code.getNumOpt2())
-				.numOpt3(code.getNumOpt3()).numOpt4(code.getNumOpt4()).numOpt5(code.getNumOpt5()).rmk(code.getRmk())
-				.build();
-	}
-
-	public TCodeD toEntity() {
-		return TCodeD.builder().pCd(pCd).cd(cd).cdNm(cdNm).useYn(useYn).sortSeq(sortSeq).wdOpt1(wdOpt1).wdOpt2(wdOpt2)
-		        .wdOpt3(wdOpt3).wdOpt4(wdOpt4).wdOpt5(wdOpt5).numOpt1(numOpt1).numOpt2(numOpt2).numOpt3(numOpt3)
-		        .numOpt4(numOpt4).numOpt5(numOpt5).rmk(rmk).crtId(getCrtId()).mdfId(getMdfId()).build();
-	}
+    private static final long serialVersionUID = 1L;
+    
+    @NotNull
+    @JsonProperty("pCd") // Json Type으로 보여질 Field명
+    private String pCd;
+    
+    @NotNull
+    private String cd;
+    
+    private String cdNm;
+    
+    @Size(min = 1, max = 1)
+    private String useYn;
+    
+    @Digits(integer = 4, fraction = 0) // 정수 4자리, 소수점 0자리
+    private Integer sortSeq;
+    
+    private String wdOpt1;
+    private String wdOpt2;
+    private String wdOpt3;
+    private String wdOpt4;
+    private String wdOpt5;
+    
+    private Integer numOpt1;
+    private Integer numOpt2;
+    private Integer numOpt3;
+    private Integer numOpt4;
+    private Integer numOpt5;
+    
+    private String rmk;
+    
+    public static TCodeDDto makeDto(TCodeD code) {
+        return TCodeDDto.builder().pCd(code.getPCd()).cd(code.getCd()).cdNm(code.getCdNm())
+                .sortSeq(code.getSortSeq())
+                .useYn(code.getUseYn()).wdOpt1(code.getWdOpt1()).wdOpt2(code.getWdOpt2())
+                .wdOpt3(code.getWdOpt3())
+                .wdOpt4(code.getWdOpt4()).wdOpt5(code.getWdOpt5()).numOpt1(code.getNumOpt1())
+                .numOpt2(code.getNumOpt2())
+                .numOpt3(code.getNumOpt3()).numOpt4(code.getNumOpt4()).numOpt5(code.getNumOpt5())
+                .rmk(code.getRmk())
+                .build();
+    }
+    
+    public TCodeD toEntity() {
+        return TCodeD.builder().pCd(pCd).cd(cd).cdNm(cdNm).useYn(useYn).sortSeq(sortSeq)
+                .wdOpt1(wdOpt1).wdOpt2(wdOpt2)
+                .wdOpt3(wdOpt3).wdOpt4(wdOpt4).wdOpt5(wdOpt5).numOpt1(numOpt1).numOpt2(numOpt2)
+                .numOpt3(numOpt3)
+                .numOpt4(numOpt4).numOpt5(numOpt5).rmk(rmk).crtId(getCrtId()).mdfId(getMdfId())
+                .build();
+    }
 }
